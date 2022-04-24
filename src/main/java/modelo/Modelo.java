3ac6ae5feadca95eae7ce1cec552938c4649edb8
diegoefbv <diegoflorez2368@gmail.com>
2022-04-24 15:43:40 -0500

    mi primer commit

---
+++ src/main/java/modelo/Modelo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author diego
+ */
+public class Modelo {
+    
+    
+    static int numero = 0;
+    private  String semaforo = "Verde";
+    
+    
+            
+}
